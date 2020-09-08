da9ac2a23f28f57077912cff0a0ff072a2b5c320
DiegoEstebanCuenca <diegoec_88@hotmail.com>
2020-09-08 01:36:18 -0500

    Udp_2

--- CursoCrudRazor/CursoCrudRazor/Pages/ListaCurso/Create.cshtml.cs
+++ CursoCrudRazor/CursoCrudRazor/Pages/ListaCurso/Create.cshtml.cs
@@ -10,11 +10,27 @@ namespace CursoCrudRazor.Pages.ListaCurso
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
         [BindProperty] //Vincular con la clase de Curso (tbl)
         public Curso Curso { get; set; }
         public void OnGet()
         {
 
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();// para retornar a la misma pagina
+            }
+            _db.Add(Curso);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
